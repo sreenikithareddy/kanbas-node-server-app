4741d192bcb29b4a5522358f8835395a2b3c2a3d
karangoyal03 <goyalkaran31@gmail.com>
2024-07-17 16:47:18 -0400

    changes for assignment screen

--- Kanbas/Assignments/routes.js
+++ Kanbas/Assignments/routes.js
@@ -1,46 +1,42 @@
-import db from "../Database/index.js";
-
-export default function AssignmentRoutes(app) {
-  // Retrieve assignments for a course
-  app.get("/api/courses/:cid/assignments", (req, res) => {
-    const { cid } = req.params;
-    const assignments = db.assignments.filter((a) => a.course === cid);
-    console.log(`Retrieved assignments for course: ${cid}`, assignments);
-    res.json(assignments);
+import assignments from "../Database/assignments.js";
+import Database from "../Database/index.js";
+export default function AssingmentsRoutes(app) {
+  app.get("/api/assignments", (req, res) => {
+    const assignments = Database.assignments;
+    res.send(assignments);
   });
 
-  // Create a new assignment for a course
-  app.post("/api/courses/:cid/assignments", (req, res) => {
-    const { cid } = req.params;
-    const newAssignment = {
-      ...req.body,
-      course: cid,
-      _id: new Date().getTime().toString(),
-    };
-    db.assignments.push(newAssignment);
-    console.log(`Created new assignment for course: ${cid}`, newAssignment);
-    res.send(newAssignment);
+  app.get("/api/assignments/:id", (req, res) => {
+    const { id } = req.params;
+    const assignments = Database.assignments.filter((a) => a.course === id);
+    res.send(assignments);
   });
 
-  // Delete an assignment
-  app.delete("/api/assignments/:aid", (req, res) => {
-    const { aid } = req.params;
-    db.assignments = db.assignments.filter((a) => a._id !== aid);
-    console.log(`Deleted assignment: ${aid}`);
-    res.sendStatus(200);
+  app.get("/api/assignment/:id", (req, res) => {
+    const { id } = req.params;
+    const assignments = Database.assignments.filter((a) => a._id === id);
+    res.send(assignments);
   });
 
-  // Update an assignment
-  app.put("/api/assignments/:aid", (req, res) => {
-    const { aid } = req.params;
-    const assignmentIndex = db.assignments.findIndex((a) => a._id === aid);
-    if (assignmentIndex !== -1) {
-      db.assignments[assignmentIndex] = { ...db.assignments[assignmentIndex], ...req.body };
-      console.log(`Updated assignment: ${aid}`, db.assignments[assignmentIndex]);
-      res.sendStatus(204);
-    } else {
-      console.log(`Assignment not found: ${aid}`);
-      res.sendStatus(404);
-    }
+  app.delete("/api/assignments/:id", (req, res) => {
+    const { id } = req.params;
+    Database.assignments = Database.assignments.filter((a) => a._id !== id);
+    res.sendStatus(204);
   });
+  app.post("/api/assignments", (req, res) => {
+    const assignment = { ...req.body,
+      _id: new Date().getTime().toString() };
+    Database.assignments.push(assignment);
+    res.send(assignment);
+  });
+
+  app.put("/api/assignments/:id", (req, res) => {
+    const { id } = req.params;
+    const assignment = req.body;
+    Database.assignments = Database.assignments.map((a) =>
+      a._id === id ? { ...a, ...assignment } : a
+    );
+    res.sendStatus(204);
+  });
+
 }
