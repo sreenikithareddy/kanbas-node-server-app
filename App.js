c750f4bce693c41a6bca7e397b7ab3ddea8b56b9
sreenikithareddy <sreenikithareddy@gmail.com>
2024-07-29 22:24:59 +0530

    A6

--- App.js
+++ App.js
@@ -1,34 +1,42 @@
-import express from 'express';
-import mongoose from "mongoose";
 import "dotenv/config";
-import Hello from "./Hello.js";
-import cors from 'cors';
+import express from 'express';
+import Hello from './Hello.js';
 import Lab5 from "./Labs/Lab5/index.js";
-import bodyParser from 'body-parser';
-import CourseRoutes from './Kanbas/Courses/routes.js';
-import ModuleRoutes from './Kanbas/Modules/routes.js';
+import cors from "cors";
+import CourseRoutes from "./Kanbas/Courses/routes.js";
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from './Kanbas/Assignments/routes.js';
+import mongoose from "mongoose";
 import UserRoutes from "./Users/routes.js";
+import session from "express-session";
 
-const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb+srv://sreenikithareddydoddareddy:Nikitha@900@atlascluster.mzbtxw.mongodb.net/kanbas?retryWrites=true&w=majority&appName=AtlasCluster";
-
-mongoose.connect(CONNECTION_STRING, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const app = express();
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
+const app = express()
+app.use(cors({
+    credentials: true,
+    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+  }
+ ));
+ const sessionOptions = {
+    secret: process.env.SESSION_SECRET || "kanbas",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.NODE_SERVER_DOMAIN,
+    };
+  }   
+  app.use(session(sessionOptions));
 app.use(express.json());
-app.use(cors());
-app.use(bodyParser.json());
-
 CourseRoutes(app);
+UserRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
-UserRoutes(app);
-Lab5(app);
-Hello(app);
-
-app.listen(process.env.PORT || 4000, () => {
-  console.log('Server is running on port 4000');
-});
+Hello(app)
+Lab5(app)
+app.listen(process.env.PORT || 4000)
